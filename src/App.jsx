8a469ae5a2054fd40efbab5c5e110af703fe241a
Segni Gutema <segnigutema9@gmail.com>
2024-02-08 01:03:13 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./styles/App.css";
+import { generalInfo, Form } from "./components/generalInfo";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <Form />
+      </div>
+      <div className="side">hi</div>
+    </>
+  );
+}
+
+export default App;
