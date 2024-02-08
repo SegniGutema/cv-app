49286ba1f205d73218d7fd6b3421f904a307e586
Segni Gutema <segnigutema9@gmail.com>
2024-02-08 16:01:29 +0300

    renamed generalInfo to personalInfo && added one file && edited personalInfo

---
+++ src/components/data.js
@@ -0,0 +1,14 @@
+const data = {
+  personalInfo: {
+    name: "segni",
+    position: "web dev",
+    email: "segnigutema9@gmail.com",
+    tel: "+251935969017",
+    address: "Addis Ababa, Ethiopia",
+    company: "self employed",
+  },
+  education: [],
+  experience: [],
+  skills: [],
+};
+export default data;
