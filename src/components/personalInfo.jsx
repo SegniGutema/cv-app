eaeb83f0ea897a835e028565e0739e3d2ec2b4f9
Segni Gutema <segnigutema9@gmail.com>
2024-02-09 09:49:35 +0300

    fixed bug in persomalInfo file

--- src/components/personalInfo.jsx
+++ src/components/personalInfo.jsx
@@ -1,70 +1,33 @@
 import { useState } from "react";
-import "../styles/education.css";
+import "../styles/personalInfo.css";
 
-function Input({ type, input, placeholder }) {
-  const [value, setValue] = useState(input);
-  function changeValue(e) {
+function Input({ placeholder }) {
+  const [value, setValue] = useState("");
+  function changeInput(e) {
     setValue(e.target.value);
   }
   return (
-    <input
-      type={type}
-      value={value}
-      onChange={changeValue}
-      placeholder={placeholder}
-    />
+    <>
+      <input
+        key={placeholder}
+        id={placeholder}
+        value={value}
+        onChange={changeInput}
+        placeholder={placeholder}
+      />
+    </>
   );
 }
-
-function InputSection({ item }) {
+export function PersonalInfo({ personalInfo }) {
   return (
-    <div className="eduCard">
-      <Input
-        type="text"
-        input={item.school}
-        placeholder={"school/university"}
-      />
-      <Input
-        type="text"
-        input={item.fieldOfStudy}
-        placeholder={"field of study"}
-      />
-      <Input type="date" input={item.startDate} placeholder={""} />
-      <Input type="date" input={item.endDate} placeholder={""} />
-    </div>
-  );
-}
-
-export function Education({ education }) {
-  const [edus, setEdus] = useState(education);
-
-  function addEdu() {
-    setEdus([
-      ...edus,
-      {
-        id: "",
-        school: "company",
-        fieldOfStudy: "position",
-        startDate: "Present",
-        endDate: "Present",
-      },
-    ]);
-  }
-
-  function deleteEdu(eduId) {
-    let newEdus = edus.filter((edu) => edu.id !== eduId);
-    setEdus(newEdus);
-  }
-
-  return (
-    <section className="education">
-      <p>Education</p>
-      <hr className="divider" />
-      <div className="eduContainer">
-        {edus.map((edu) => (
-          <InputSection item={edu} />
-        ))}
-      </div>
+    <section className="personalInfo">
+      <Input placeholder={"Name"} />
+      <Input placeholder={"Position"} />
+      <br />
+      <Input placeholder={"Company"} />
+      <Input placeholder={"Email"} />
+      <Input placeholder={"Tel"} />
+      <Input placeholder={"Address"} />
     </section>
   );
 }
